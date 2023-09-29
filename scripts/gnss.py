24e67549025554236e0beea5b30b443c63a9fce3
Gennaro Raiola <gennaro.raiola@gmail.com>
2023-09-29 12:38:52 +0200

    Fix tabs and add tf_prefix

--- scripts/gnss.py
+++ scripts/gnss.py
@@ -13,50 +13,57 @@ ser = serial.Serial('/dev/ttyUSB2', baudrate=115200, timeout=1)
 
 
 def send_command(command):
-    ser.write(command.encode() + b'\r\n')
-    time.sleep(1)
-    return ser.readlines()
+	ser.write(command.encode() + b'\r\n')
+	time.sleep(1)
+	return ser.readlines()
 
 
 if __name__ == '__main__':
 
-    rospy.init_node("gps_fix_pub")
+	rospy.init_node("gps_fix_pub")
 
-    gps_pub = rospy.Publisher("gps/fix", NavSatFix, queue_size=10)
+	gps_pub = rospy.Publisher("gps/fix", NavSatFix, queue_size=10)
+	rospy.get_param("~tf_prefix", tf_prefix, "")
 
-    send_command('AT')
+	# Check and fix tf_prefix
+	if tf_prefix == "/":
+		tf_prefix = ""
+	elif len(tf_prefix) != 0:
+		tf_prefix = tf_prefix + "/"
 
-    # Activate GPS module
-    send_command('AT+QGPS=1')
+	send_command('AT')
 
-    gps_msg = NavSatFix()
-    gps_msg.header.frame_id = "trunk"
+	# Activate GPS module
+	send_command('AT+QGPS=1')
 
-    while not rospy.is_shutdown():
-        # Get GPS coordinates
-        response = send_command('AT+QGPSLOC=1')
-        # pdb.set_trace()
-        latitude = ""
-        longitude = ""
-        altitude = ""
+	gps_msg = NavSatFix()
+	gps_msg.header.frame_id = tf_prefix+"trunk"
 
-        # Read the answer and search for GPS coordinates
-        for line in response:
-            line = line.decode().strip()
-            if line.startswith('+QGPSLOC'):
-                parts = line.split(',')
-                if len(parts) >= 6:
-                    latitude = parts[1]
-                    longitude = parts[3]
-                    altitude = parts[5]
-                    print('Latitude: %s, Longitude: %s, Altitude: %s' % (latitude, longitude, altitude))
+	while not rospy.is_shutdown():
+		# Get GPS coordinates
+		response = send_command('AT+QGPSLOC=1')
+		# pdb.set_trace()
+		latitude = ""
+		longitude = ""
+		altitude = ""
 
-        if latitude != "":
-            gps_msg.latitude = float(latitude) / 100
-            gps_msg.longitude = float(longitude) / 100
-            gps_msg.altitude = float(altitude)
-            gps_msg.header.stamp = rospy.Time.now()
+		# Read the answer and search for GPS coordinates
+		for line in response:
+			line = line.decode().strip()
+			if line.startswith('+QGPSLOC'):
+				parts = line.split(',')
+				if len(parts) >= 6:
+					latitude = parts[1]
+					longitude = parts[3]
+					altitude = parts[5]
+					print('Latitude: %s, Longitude: %s, Altitude: %s' % (latitude, longitude, altitude))
 
-            gps_pub.publish(gps_msg)
-    # Close the serial port
-    ser.close()
+		if latitude != "":
+			gps_msg.latitude = float(latitude) / 100
+			gps_msg.longitude = float(longitude) / 100
+			gps_msg.altitude = float(altitude)
+			gps_msg.header.stamp = rospy.Time.now()
+
+			gps_pub.publish(gps_msg)
+	# Close the serial port
+	ser.close()
