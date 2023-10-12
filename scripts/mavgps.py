6c5ebee8e3a45787fafcf0288a403defb200e9fc
graiola <gennaro.raiola@gmail.com>
2023-10-12 15:06:54 +0200

    Add publisher in mavgps

--- scripts/mavgps.py
+++ scripts/mavgps.py
@@ -1,13 +1,13 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 """
 read GPS raw from PX4
 """
+
 from __future__ import print_function
 from pymavlink import mavutil
-
-device = "/dev/serial/by-id/usb-Auterion_PX4_FMU_v6X.x_0-if00"
-baudrate = 57600
+import rospy
+from sensor_msgs.msg import NavSatFix
 
 """
 Mavlink messages in https://mavlink.io/en/messages/common.html
@@ -31,14 +31,38 @@ hdg_acc : Heading / track uncertainty # degE5
 yaw : Yaw in earth frame from north # cdeg
 """
 
-mlog = mavutil.mavlink_connection(device, autoreconnect=True, baud=baudrate)
+# Check https://docs.px4.io/main/en/companion_computer/pixhawk_companion.html
+device = "/dev/ttyPixhawk"
+baudrate = 57600
 
-while True:
+if __name__ == '__main__':
+
+    rospy.init_node("mavgps_pub")
+
+    tf_prefix = rospy.get_param('~tf_prefix', '')
+
+    # Check and fix tf_prefix
+    if tf_prefix == "/":
+        tf_prefix = ""
+    elif len(tf_prefix) != 0:
+        tf_prefix = tf_prefix + "/"
+
+    print('Launch mavgps_pub with tf_prefix="%s"' % (tf_prefix))
+
+    gps_pub = rospy.Publisher(tf_prefix + "gps/fix", NavSatFix, queue_size=10)
+
+    mlog = mavutil.mavlink_connection(device, autoreconnect=True, baud=baudrate)
+
+    gps_msg = NavSatFix()
+    gps_msg.header.frame_id = tf_prefix + "trunk"
+
+    while not rospy.is_shutdown():
     
-    m = mlog.recv_match(type="GPS_RAW_INT", blocking=True)
+        msg = mlog.recv_match(type="GPS_RAW_INT", blocking=False)
 
-    if m is not None:
-        print(m)
-        print("\n")
-        print(m.lat)
-        print("\n")
+        if msg is not None:
+            print('Latitude: %s, Longitude: %s, Altitude: %s' % (msg.lat, msg.lon, msg.alt))
+            gps_msg.latitude = float(msg.lat) / 100
+            gps_msg.longitude = float(msg.lon) / 100
+            gps_msg.altitude = float(msg.alt)
+            gps_msg.header.stamp = rospy.Time.now()
